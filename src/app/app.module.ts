933c3f27bb4fee5636a101e2be76d03d1fcf08ce
roziana-rodrigues <roziana.rodrigues@terrainvestimentos.com.br>
2023-11-09 17:02:08 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoverPageComponent } from './components/cover-page/cover-page.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoverPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
